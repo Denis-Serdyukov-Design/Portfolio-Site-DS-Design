444a5a384271e512e1fa89b978abe1645f21c4e4
Denis-Serdyukov-Design <67653678+Denis-Serdyukov-Design@users.noreply.github.com>
2020-08-03 16:27:29 -0500

    Correct spacing/transition, script local nav menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 console.log("Hello there, General Kenobi!");
 
 $(".ask-away-button").on("click", function() {
-  alert("Ask Away button has been clicked!")
+  alert("Coming soon! This page is still under construction!")
 });
 
 var x = document.getElementById('myNavToggle');
@@ -25,6 +25,140 @@ function menuToggle () {
     x.className = 'navtoggle';
   }
 }
+
+
+
+
+//Activate local nav links based on scroll location on page.
+
+$(window).scroll(function() {
+		var scrollDistance = $(window).scrollTop();
+    var scrollDistanceAdjusted = scrollDistance + 5;
+
+		// Assign active class to nav links while scolling
+		$('.page-section').each(function(i) {
+				if ($(this).position().top <= scrollDistanceAdjusted) {
+            $('.local-nav-menu a i').removeClass('fas fa-circle');
+            $('.local-nav-menu a i').addClass('far fa-circle');
+            $('.local-nav-menu a i').eq(i).removeClass('far fa-circle');
+            $('.local-nav-menu a i').eq(i).addClass('fas fa-circle');
+            $('.local-nav-menu a span').removeClass('ln-active');
+            $('.local-nav-menu a span').eq(i).addClass('ln-active');
+						//$('.local-nav-menu a.active').removeClass('active');
+						//$('.local-nav-menu a').eq(i).addClass('active');
+				}
+		});
+}).scroll();
+
+
+
+//Activate local nav links based on mouse over.
+
+var AlreadyActive;
+
+$('#lnl1').mouseover(function(){
+  if ($('#span1').hasClass('ln-active')) {
+       AlreadyActive = true;
+       console.log("Already active");
+   } else {
+     AlreadyActive = false;
+     console.log("Not active")
+     $('#span1').addClass('ln-active');
+   }
+});
+
+$('#lnl1').mouseout(function(){
+  if (AlreadyActive === true) {
+    //Do nothing
+  } else {
+    $('#span1').removeClass('ln-active');
+  }
+});
+
+
+
+$('#lnl2').mouseover(function(){
+  if ($('#span2').hasClass('ln-active')) {
+       AlreadyActive = true;
+       console.log("Already active");
+   } else {
+     AlreadyActive = false;
+     console.log("Not active")
+     $('#span2').addClass('ln-active');
+   }
+});
+
+$('#lnl2').mouseout(function(){
+  if (AlreadyActive === true) {
+    //Do nothing
+  } else {
+    $('#span2').removeClass('ln-active');
+  }
+});
+
+
+
+$('#lnl3').mouseover(function(){
+  if ($('#span3').hasClass('ln-active')) {
+       AlreadyActive = true;
+       console.log("Already active");
+   } else {
+     AlreadyActive = false;
+     console.log("Not active")
+     $('#span3').addClass('ln-active');
+   }
+});
+
+$('#lnl3').mouseout(function(){
+  if (AlreadyActive === true) {
+    //Do nothing
+  } else {
+    $('#span3').removeClass('ln-active');
+  }
+});
+
+
+
+$('#lnl4').mouseover(function(){
+  if ($('#span4').hasClass('ln-active')) {
+       AlreadyActive = true;
+       console.log("Already active");
+   } else {
+     AlreadyActive = false;
+     console.log("Not active")
+     $('#span4').addClass('ln-active');
+   }
+});
+
+$('#lnl4').mouseout(function(){
+  if (AlreadyActive === true) {
+    //Do nothing
+  } else {
+    $('#span4').removeClass('ln-active');
+  }
+});
+
+
+
+$('#lnl5').mouseover(function(){
+  if ($('#span5').hasClass('ln-active')) {
+       AlreadyActive = true;
+       console.log("Already active");
+   } else {
+     AlreadyActive = false;
+     console.log("Not active")
+     $('#span5').addClass('ln-active');
+   }
+});
+
+$('#lnl5').mouseout(function(){
+  if (AlreadyActive === true) {
+    //Do nothing
+  } else {
+    $('#span5').removeClass('ln-active');
+  }
+});
+
 
 
 //Show header when scrolling up, hide when scrolling down.
