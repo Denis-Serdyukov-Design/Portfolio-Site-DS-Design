b015492b9b7947c5cbe4a85b01b427af42f52d4d
Denis-Serdyukov-Design <67653678+Denis-Serdyukov-Design@users.noreply.github.com>
2020-08-11 16:56:54 -0500

    Refine code quality/correct errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,194 +1,172 @@
-console.log("Hello there, General Kenobi!");
+/* global $, */
+// console.log("Hello there, General Kenobi!");
 
-$(".ask-away-button").on("click", function() {
-  alert("Coming soon! This page is still under construction!")
-});
+$('.ask-away-button').on('click', function () {
+  window.alert('Coming soon! This page is still under construction!')
+})
 
-var x = document.getElementById('myNavToggle');
+var x = document.getElementById('myNavToggle')
 
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navBarHeight = $('header').outerHeight();
+var didScroll
+var lastScrollTop = 0
+var delta = 5
+var navBarHeight = window.$('header').outerHeight()
 
-console.log (navBarHeight);
+console.log(navBarHeight)
 
-
-function menuToggle () {
-  //alert("Menu has been clicked!");
-  // var x = document.getElementById('myNavToggle');
+function menuToggle () { // eslint-disable-line no-unused-vars
   if (x.className === 'navtoggle') {
-    console.log ("Expanding.");
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    console.log ("Collapsing.");
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
 
+// Activate local nav links based on scroll location on page.
 
+$(window).scroll(function () {
+  var scrollDistance = $(window).scrollTop()
+  var scrollDistanceAdjusted = scrollDistance + 5
 
+  // Assign active class to nav links while scolling
+  $('.page-section').each(function (i) {
+    if ($(this).position().top <= scrollDistanceAdjusted) {
+      $('.local-nav-menu a i').removeClass('fas fa-circle')
+      $('.local-nav-menu a i').addClass('far fa-circle')
+      $('.local-nav-menu a i').eq(i).removeClass('far fa-circle')
+      $('.local-nav-menu a i').eq(i).addClass('fas fa-circle')
+      $('.local-nav-menu a span').removeClass('ln-active')
+      $('.local-nav-menu a span').eq(i).addClass('ln-active')
+    }
+  })
+}).scroll()
 
-//Activate local nav links based on scroll location on page.
+// Activate local nav links based on mouse over.
 
-$(window).scroll(function() {
-		var scrollDistance = $(window).scrollTop();
-    var scrollDistanceAdjusted = scrollDistance + 5;
+var AlreadyActive
 
-		// Assign active class to nav links while scolling
-		$('.page-section').each(function(i) {
-				if ($(this).position().top <= scrollDistanceAdjusted) {
-            $('.local-nav-menu a i').removeClass('fas fa-circle');
-            $('.local-nav-menu a i').addClass('far fa-circle');
-            $('.local-nav-menu a i').eq(i).removeClass('far fa-circle');
-            $('.local-nav-menu a i').eq(i).addClass('fas fa-circle');
-            $('.local-nav-menu a span').removeClass('ln-active');
-            $('.local-nav-menu a span').eq(i).addClass('ln-active');
-						//$('.local-nav-menu a.active').removeClass('active');
-						//$('.local-nav-menu a').eq(i).addClass('active');
-				}
-		});
-}).scroll();
-
-
-
-//Activate local nav links based on mouse over.
-
-var AlreadyActive;
-
-$('#lnl1').mouseover(function(){
+$('#lnl1').mouseover(function () {
   if ($('#span1').hasClass('ln-active')) {
-       AlreadyActive = true;
-       console.log("Already active");
-   } else {
-     AlreadyActive = false;
-     console.log("Not active")
-     $('#span1').addClass('ln-active');
-   }
-});
-
-$('#lnl1').mouseout(function(){
-  if (AlreadyActive === true) {
-    //Do nothing
+    AlreadyActive = true
+    console.log('Already active')
   } else {
-    $('#span1').removeClass('ln-active');
+    AlreadyActive = false
+    console.log('Not active')
+    $('#span1').addClass('ln-active')
   }
-});
+})
 
+$('#lnl1').mouseout(function () {
+  if (AlreadyActive === true) {
+    // Do nothing
+  } else {
+    $('#span1').removeClass('ln-active')
+  }
+})
 
-
-$('#lnl2').mouseover(function(){
+$('#lnl2').mouseover(function () {
   if ($('#span2').hasClass('ln-active')) {
-       AlreadyActive = true;
-       console.log("Already active");
-   } else {
-     AlreadyActive = false;
-     console.log("Not active")
-     $('#span2').addClass('ln-active');
-   }
-});
-
-$('#lnl2').mouseout(function(){
-  if (AlreadyActive === true) {
-    //Do nothing
+    AlreadyActive = true
+    console.log('Already active')
   } else {
-    $('#span2').removeClass('ln-active');
+    AlreadyActive = false
+    console.log('Not active')
+    $('#span2').addClass('ln-active')
   }
-});
+})
 
+$('#lnl2').mouseout(function () {
+  if (AlreadyActive === true) {
+    // Do nothing
+  } else {
+    $('#span2').removeClass('ln-active')
+  }
+})
 
-
-$('#lnl3').mouseover(function(){
+$('#lnl3').mouseover(function () {
   if ($('#span3').hasClass('ln-active')) {
-       AlreadyActive = true;
-       console.log("Already active");
-   } else {
-     AlreadyActive = false;
-     console.log("Not active")
-     $('#span3').addClass('ln-active');
-   }
-});
-
-$('#lnl3').mouseout(function(){
-  if (AlreadyActive === true) {
-    //Do nothing
+    AlreadyActive = true
+    console.log('Already active')
   } else {
-    $('#span3').removeClass('ln-active');
+    AlreadyActive = false
+    console.log('Not active')
+    $('#span3').addClass('ln-active')
   }
-});
+})
 
+$('#lnl3').mouseout(function () {
+  if (AlreadyActive === true) {
+    // Do nothing
+  } else {
+    $('#span3').removeClass('ln-active')
+  }
+})
 
-
-$('#lnl4').mouseover(function(){
+$('#lnl4').mouseover(function () {
   if ($('#span4').hasClass('ln-active')) {
-       AlreadyActive = true;
-       console.log("Already active");
-   } else {
-     AlreadyActive = false;
-     console.log("Not active")
-     $('#span4').addClass('ln-active');
-   }
-});
-
-$('#lnl4').mouseout(function(){
-  if (AlreadyActive === true) {
-    //Do nothing
+    AlreadyActive = true
+    console.log('Already active')
   } else {
-    $('#span4').removeClass('ln-active');
+    AlreadyActive = false
+    console.log('Not active')
+    $('#span4').addClass('ln-active')
   }
-});
+})
 
+$('#lnl4').mouseout(function () {
+  if (AlreadyActive === true) {
+    // Do nothing
+  } else {
+    $('#span4').removeClass('ln-active')
+  }
+})
 
-
-$('#lnl5').mouseover(function(){
+$('#lnl5').mouseover(function () {
   if ($('#span5').hasClass('ln-active')) {
-       AlreadyActive = true;
-       console.log("Already active");
-   } else {
-     AlreadyActive = false;
-     console.log("Not active")
-     $('#span5').addClass('ln-active');
-   }
-});
-
-$('#lnl5').mouseout(function(){
-  if (AlreadyActive === true) {
-    //Do nothing
+    AlreadyActive = true
+    console.log('Already active')
   } else {
-    $('#span5').removeClass('ln-active');
+    AlreadyActive = false
+    console.log('Not active')
+    $('#span5').addClass('ln-active')
   }
-});
+})
 
+$('#lnl5').mouseout(function () {
+  if (AlreadyActive === true) {
+    // Do nothing
+  } else {
+    $('#span5').removeClass('ln-active')
+  }
+})
 
+// Show header when scrolling up, hide when scrolling down.
 
-//Show header when scrolling up, hide when scrolling down.
+$(window).scroll(function (event) {
+  didScroll = true
+})
 
-$(window).scroll(function(event) {
-  didScroll = true;
-});
-
-setInterval(function(){
+setInterval(function () {
   if (didScroll) {
-    hasScrolled();
-    didScroll = false;
+    hasScrolled()
+    didScroll = false
   }
-}, 250);
+}, 250)
 
-function hasScrolled() {
-  var st = $(this).scrollTop();
-  if (Math.abs(lastScrollTop-st) <= delta)
-    return;
+function hasScrolled () {
+  var st = $(this).scrollTop()
+  if (Math.abs(lastScrollTop - st) <= delta) {
+    return
+  }
 
   if (st > lastScrollTop && st > navBarHeight) {
-    //Scroll down event.
-    $('header').removeClass('nav-down').addClass('nav-up');
-    //$('main').removeClass('add-space').addClass('no-space');
-    x.className = 'navtoggle';
+    // Scroll down event.
+    $('header').removeClass('nav-down').addClass('nav-up')
+    x.className = 'navtoggle'
   } else {
-    //Scroll up event.
-    $('header').removeClass('nav-up').addClass('nav-down');
-    //$('main').removeClass('no-space').addClass('add-space');
+    // Scroll up event.
+    $('header').removeClass('nav-up').addClass('nav-down')
   }
 
-  lastScrollTop = st;
+  lastScrollTop = st
 }
